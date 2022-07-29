2650e42ac5917bf2a47859782d90aa3a92e9f6e7
AD <8b4ll@gmx.de>
2022-07-29 16:02:06 +0200

    Init

---
+++ pages/test.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+	h1 Message
+		.msg {{ msg }}
+</template>
+
+<script lang="ts">
+	export default {
+		data: function () {
+			return { msg: String };
+		},
+		mounted() {
+			this.msg = 'Hello World!!!';
+		},
+	};
+</script>
+
+<style lang="sass">
+	h1
+		color: blue
+	.msg
+		color: red
+</style>
