e962ea70e9189fd081c1dd5d9f75cc5b50bb0f5b
AD <8b4ll@gmx.de>
2022-10-07 09:09:49 +0200

    UPDATE Showcase

--- pages/test.vue
+++ pages/test.vue
@@ -1,22 +1,29 @@
 <template lang="pug">
-	h1 Message
-		.msg {{ msg }}
+h1.blue--text Message
+	.red--text {{ msg }}
+	v-btn(icon outlined color="primary")
+		v-icon mdi-heart
+	v-color-picker(hide-inputs)
 </template>
 
 <script lang="ts">
-	export default {
+	import Vue from 'vue';
+	export default Vue.extend({
 		data: function () {
-			return { msg: String };
+			return { msg: new String() };
 		},
 		mounted() {
-			this.msg = 'Hello World!!!';
+			this.msg = 'HELLO World !!';
 		},
-	};
+	});
 </script>
 
-<style lang="sass">
+<style scoped lang="sass">
 	h1
 		color: blue
 	.msg
 		color: red
+	v-color-picker
+		input
+			font-size: .1em
 </style>
